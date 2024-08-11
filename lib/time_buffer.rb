152f3f5ad784e59788fdd08bd9f4d569beb7e870
Steven Elberger <stevenelberger@gmail.com>
2024-08-10 17:36:14 -0700

    Initialize gem

---
+++ lib/time_buffer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "time_buffer/version"
+
+module TimeBuffer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/time_buffer_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe TimeBuffer do
+  it "has a version number" do
+    expect(TimeBuffer::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
