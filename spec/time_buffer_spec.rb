1e38786e596cb67d5797a246f5b80963d248e55d
Steven Elberger <stevenelberger@gmail.com>
2024-08-18 23:30:53 -0700

    Remove useless spec

--- spec/time_buffer_spec.rb
+++ spec/time_buffer_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe TimeBuffer do
   it "has a version number" do
     expect(TimeBuffer::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
